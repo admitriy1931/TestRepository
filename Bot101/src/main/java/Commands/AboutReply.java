4d4806501ddb789750be1a646e33e3598b2df933
SlivnyiArtem <slivnay1965@gmail.com>
2021-11-23 00:24:19 +0500

    changes part 1

--- Bot101/src/main/java/Commands/AboutReply.java
+++ Bot101/src/main/java/Commands/AboutReply.java
@@ -1,8 +1,8 @@
 package commands;
 
-public class AboutCommand implements SimpleBotCommand {
+public class AboutCommand implements DialogAnswer {
     @Override
-    public String returnAnswer(String string) {
+    public String getContent() {
         return "Я - Бот для Telegram, написанный на java. " +
                 "Комманда разработки: Агафонов Дмитрий, Сливный Артём";
     }
