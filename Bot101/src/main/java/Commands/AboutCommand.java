04bfbbeb4ba6b97fad55ee6ba4c005e87178be5a
SlivnyiArtem <slivnay1965@gmail.com>
2021-12-17 20:44:04 +0500

    add simple audio playing functional

--- Bot101/src/main/java/Commands/AboutCommand.java
+++
@@ -1,9 +0,0 @@
-package commands;
-
-public class AboutCommand implements SimpleBotCommand {
-    @Override
-    public String returnAnswer() {
-        return "Я - Бот для Telegram, написанный на java. " +
-                "Комманда разработки: Агафонов Дмитрий, Сливный Артём";
-    }
-}
