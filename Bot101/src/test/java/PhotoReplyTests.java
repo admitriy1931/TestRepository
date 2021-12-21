44f430956958cbdb31358ba405147660d3960a93
admitriy1931 <82140063+admitriy1931@users.noreply.github.com>
2021-12-21 11:34:53 +0500

    new Test

--- Bot101/src/test/java/PhotoReplyTests.java
+++ Bot101/src/test/java/PhotoReplyTests.java
@@ -1,10 +1,31 @@
 import imageReplyes.GeoMap;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PhotoReplyTests {
-
+    @Test
+    void PhotoTest() throws IOException {
+        var actual = new GeoMap().getReply("http://openweathermap.org/img/wn/13d@2x.png",
+                61.38593673706055,55.15963363647461);
+        File file = new File("C:\\bytes.txt");
+        FileReader fr = new FileReader(file);
+        BufferedReader reader = new BufferedReader(fr);
+        String line = reader.readLine();
+        var list = new ArrayList<>(List.of(line.split(",")));
+        var arrayList = new ArrayList<String>();
+        for (var e:actual
+             ) {
+            String l = (String.valueOf(e));
+            arrayList.add(l);
+        }
+        Collections.sort(list);
+        Collections.sort(arrayList);
+        assertEquals(list,arrayList);
+    }
 }
